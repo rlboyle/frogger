8ecabb0dc0ee80c0ec94e8bafdd5373219c91255
Ryan Boyle <146665483+rlboyle@users.noreply.github.com>
2024-05-07 15:57:06 -0500

    Update Frogger_final.s

--- Frogger_final.s
+++ Frogger_final.s
@@ -1,28 +1,3 @@
-// Ryan Boyle RLB7543
-
-// Inspired by the classic arcade game frogger, the player controls the frog
-// 		using the pushbuttons and accumulates points by moving forward.
-
-// Advanced Features 
-// Sound: Intro music, sound when frog moves, frog dies, powerup collected, and level completed 
-// Massive number of active and discernable sprites: 1 frog, 1 moving text sprite, 3 powerups, 21 cars
-// Complex scoring system: Increment score by ten when frog moves forward, moving backwards
-// 		and forwards again does not count. Points only accumulate when the frog moves to a
-//		previously unreached y-coordinate. Powerup #1 (pink square) adds one life when collected.
-//		Powerup #2 (yellow square) adds 100 points when collected. Evil powerup (red square)
-// 		subtracts 100 points when hit.
-
-// Controls
-// Button #0: Move frog right
-// Button #1: Move frog left
-// Button #2: Move frog down
-// Button #3: Move frog up
-// Note: Buttons must be unchecked before use. Unchecking a checked button will not move the frog.
-//		The frog can only be moved by checking an unchecked button.
-
-// YouTube Demo Link: https://youtu.be/5EHK2Ya0CcE
-
-// These are addresses for the pixel buffer and text buffer
 .EQU PIX_BUFFER, 0xc8000000
 .EQU TEXT_BUFFER, 0xc9000000
 .EQU BACK_BUFFER, 0xc0000000
@@ -34,7 +9,7 @@
 .EQU STATUS_REG, 0xff20302c
 .EQU CHAR_STATUS_REG, 0xff20303c
 
-// These are some useful defines that will help you access structure fields
+// These are some useful defines that will help access structure fields
 .EQU PIXMAP_WIDTH, 0
 .EQU PIXMAP_HEIGHT, 2
 .EQU PIXMAP_TRANSPARENCY, 4
